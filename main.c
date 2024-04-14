7858876c539b8521761af311dad04363fc6cee8d
Jesse Florig <jesseflorig@gmail.com>
2024-04-14 13:21:57 -0400

    updated consts and comments

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_NUMS 10
+#define MAX_NUM_LENGTH 100
+#define MAX_NUM_COUNT 100
 
 bool isNumber(const char *input){
     for (int i = 0; input[i] != '\0'; i++) { //Iterate through each character
@@ -16,15 +17,15 @@ bool isNumber(const char *input){
 }
 
 bool isOperator(const char *input, const char value){
-  if (input[0] == value) {
+  if (input[0] == value) { //Check if first character is passed value
       return true;
   }
   return false;
 }
 
 int main() {
-    char input[100];  //Store user input
-    int numbers[100]; //Store numbers input
+    char input[MAX_NUM_LENGTH];  //Store user input
+    int numbers[MAX_NUM_COUNT]; //Store numbers input
     int numSize = 0;
     bool done = false;
 
@@ -34,23 +35,23 @@ int main() {
       fgets(input, sizeof(input), stdin); //Read user input from standard input
       input[strcspn(input, "\n")] = '\0';
 
-      if (isNumber(input)){
+      if (isNumber(input)){ //If input is a number
         int newNumber = atoi(input);
-        numbers[numSize++] = newNumber;
+        numbers[numSize++] = newNumber; //Push the number to the array
         printf("You entered a number: %d\n", newNumber);
       }
-      else if (isOperator(input, '+')) {
+      else if (isOperator(input, '+')) { //If plus sign is entered
         int sum = 0;
         for (int i = 0; i < numSize; i++) {
-          sum += numbers[i];
+          sum += numbers[i]; //Sum the array
         }
         printf("The sum is: %d\n", sum);
         return 1;
       }
-      else if (isOperator(input, '*')) {
+      else if (isOperator(input, '*')) { //If multiply is entered
         int product = 1;
         for (int i = 0; i < numSize; i++) {
-          product *= numbers[i];
+          product *= numbers[i]; //Product the array
         }
         printf("The product is: %d\n", product);
         return 1;
